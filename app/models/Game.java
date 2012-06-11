ab16c9c1ed82f4a9c536af6a47b0b8f8e8e798af
lucas <lucasapa.sec@gmail.com>
2012-06-11 20:12:59 -0300

    small bug

--- app/models/Game.java
+++ app/models/Game.java
@@ -36,8 +36,8 @@ public class Game {
     }
 
     private void waitForStrategy() {
-        message(playerOne, "infoatt", "Select your strategy, game starts in 30 seconds...");
-        message(playerTwo, "infoatt", "Select your strategy, game starts in 30 seconds...");
+        message(playerOne, "attinfo", "Select your strategy, game starts in 30 seconds...");
+        message(playerTwo, "attinfo", "Select your strategy, game starts in 30 seconds...");
         synchronized (this) {
             try {
                 this.wait(30000);
